a7214532bdf905b55bdce8f588223885d32b4eb6
susmita0121 <sushh2123@gmail.com>
2026-03-17 09:12:10 +0530

    UC1

---
+++ App/src/HotelBookingApp.java
@@ -0,0 +1,16 @@
+public class HotelBookingApp {
+
+
+        public static void main(String[] args) {
+
+            System.out.println("=======================================");
+            System.out.println("     Welcome to Book My Stay App       ");
+            System.out.println("=======================================");
+            System.out.println("Application: Hotel Booking System");
+            System.out.println("Version: 1.0");
+            System.out.println("Status: Started Successfully");
+            System.out.println("=======================================");
+
+        }
+    }
+
